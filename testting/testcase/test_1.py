bb6f4c88070af08c776325492a7fdaf662a29f38
BrightS_Li <594574997@qq.com>
2020-06-10 16:31:45 +0800

    1

--- testting/testcase/test_1.py
+++ testting/testcase/test_1.py
@@ -6,8 +6,9 @@ from configFile import filedir
 
 test_data = ReadExcel(filedir.TEXT_EXCEL,'Sheet1').get_excel()
 
-
-test_1_title = SendRequest().get_test_msg(test_data[2])
-test_1 = SendRequest().get_request(test_data[2])
-print('测试模块: ' + test_1_title['test_module'] + '\n测试编号: ' + str(test_1_title['test_number']) + '\n测试标题: ' + test_1_title['test_title'] + '\n预期结果: ' + test_1_title['test_expected_result'])
-print((eval(test_1_title['test_expected_result']))['code'])
+for i in range(1,6):
+    test_1_title = SendRequest().get_test_msg(test_data[i])
+    test_1 = SendRequest().get_request(test_data[i])
+    print(test_1['code'])
+    print('测试模块: ' + test_1_title['test_module'] + '\n测试编号: ' + str(test_1_title['test_number']) + '\n测试标题: ' + test_1_title['test_title'] + '\n预期结果: ' + test_1_title['test_expected_result'])
+    print((eval(test_1_title['test_expected_result']))['code'])
